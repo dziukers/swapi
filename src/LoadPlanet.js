cf4bb9bbb15ab83e908fccd9e01abeaa1b87c9b8
Krystian Dziuk <dziukers@gmail.com>
2018-07-31 17:46:02 +0200

    planet popup

--- src/LoadPlanet.js
+++ src/LoadPlanet.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import Popup from 'reactjs-popup';
 
 class planet extends Component {
     constructor(props){
         super(props);
         this.state = {
-            homeworld: ''
+            homeworld: '',
+            terrain: '',
+            climate: '',
+            diameter: '',       
+            gravity: ''
+            
         }
     }
     
@@ -19,11 +25,15 @@ class planet extends Component {
  const homeworld = await resp.json();
 let planetName = homeworld;
  await this.setState({
-     homeworld: planetName.name
+     homeworld: planetName.name,
+     climate: planetName.climate,
+     gravity: planetName.gravity,
+     terrain: planetName.terrain,
+     diameter: planetName.diameter
  });
     }
    render(){
-       const {homeworld} = this.state;
+       const {homeworld, climate, gravity, terrain, diameter} = this.state;
        if(homeworld ===''){
            return <h1>Loading</h1>
        }
@@ -31,19 +41,44 @@ let planetName = homeworld;
 
     try{
         return (
+            <Popup
+            trigger= {
             <div>
                 <h2>{homeworld}</h2>
                 <img src={require(`./Images/${homeworld}.png`)} width='60%' alt='homeworld'/>
             </div>
+            }
+            contentStyle = {{background:'black'}}
+            position='top center'
+            on='hover'
+            >
+            <div>
+                <p>Climate: {climate}</p>
+                <hr/>
+                <p>Gravity: {gravity}</p>
+                <hr/>
+                <p>Terrain: {terrain}</p>
+                <hr/>
+                <p>Diameter: {diameter}</p>
+            </div>
+            </Popup>
             )
         }
      catch(err) {
          return(
+        <Popup
+        trigger= {
              <div style={{position:'relative'}}>
-        <h2>{homeworld}</h2>
-        <img src={require(`./Images/Alderaan.png`)} width='60%' style={{filter: 'grayscale()'}} alt='homeworld'/>
-        <h1 className='Popup-questionMark'>?</h1> 
+                <h2>{homeworld}</h2>
+                <img src={require(`./Images/Alderaan.png`)} width='60%' style={{filter: 'grayscale()'}} alt='homeworld'/>
+                <h1 className='Popup-questionMark'>?</h1> 
             </div>
+            }
+            contentStyle = {{background:'black'}}
+            position='top center'
+            on='hover'>
+                {climate}
+            </Popup>
         )
         }   
     }
