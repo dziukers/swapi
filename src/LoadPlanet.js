79279d22d2d3619e48c9821f71b0b5097042c18b
Krystian Dziuk <dziukers@gmail.com>
2018-08-02 14:14:49 +0200

    fix

--- src/LoadPlanet.js
+++ src/LoadPlanet.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import './App.css';
+import './LoadPlanet.css';
+import Popup from 'reactjs-popup';
+import PlanetInfo from './PlanetInfo';
+import gasgiant from './Images/gasgiant.jpg';
+import desert from './Images/desert.jpg';
+import urban from './Images/urban.jpg';
+import grasslands from './Images/grasslands.jpg';
+import mountainranges from './Images/mountainranges.jpg';
+import grassyhills from './Images/grassyhills.jpg';
+import ocean from './Images/ocean.jpg';
+import islands from './Images/islands.jpg';
+
+
 
 class planet extends Component {
     constructor(props){
         super(props);
         this.state = {
-            homeworld: ''
+            homeworld: '',
+            terrain: '',
+            climate: '',
+            diameter: '',       
+            gravity: ''
+            
         }
     }
     
@@ -17,39 +34,98 @@ class planet extends Component {
         
  const resp = await fetch(this.props.planet);
  const homeworld = await resp.json();
-let planetName = homeworld.name;
+let planetName = homeworld;
  await this.setState({
-     homeworld: planetName
- });
+     homeworld: planetName.name,
+     climate: planetName.climate,
+     gravity: planetName.gravity,
+     terrain: planetName.terrain,
+     diameter: planetName.diameter
+ });}
+
+    pictureName = () => {
+        const {terrain} = this.state;
+            if (terrain.includes('gas giant')) { return gasgiant};
+            if (terrain.includes('desert')) { return desert};
+            if (terrain.includes('urban') || terrain.includes('cityscape')) { return urban};
+            if (terrain.includes('grasslands')) { return grasslands};
+            if (terrain.includes('mountain ranges')) { return mountainranges};
+            if (terrain.includes('forests')) { return grassyhills};
+            if (terrain.includes('ocean')) { return ocean};
+            if (terrain.includes('islands')) { return islands};
+    }   
+
+    planetBackground = (popupBackground) => {
+        let bgStyle;
+        if (popupBackground === undefined) {
+            bgStyle = {
+                backgroundColor: 'black',
+                color: 'white',
+                border:'2px solid transparent',
+                borderImage: 'linear-gradient(to left,#aaa, #222, #aaa) 30 stretch',
+            }
+        }else {
+            bgStyle = {
+            background:'transparent',
+            backgroundImage:`linear-gradient(to bottom, rgba(255,255,255,0.3) 0%,rgba(255,255,255,0.3) 100%)
+            ,url(${popupBackground})`,
+            backgroundSize:'cover',
+            border:'2px solid transparent',
+            borderImage: 'linear-gradient(to left,#aaa, #222, #aaa) 30 stretch',
+        }
+        }
+        return bgStyle;
     }
+
    render(){
-       const {homeworld} = this.state;
+       const {homeworld, climate, gravity, terrain, diameter} = this.state;
+       const popupBackground = this.pictureName();
+       const backgroundStyle = this.planetBackground(popupBackground);
        if(homeworld ===''){
            return <h1>Loading</h1>
        }
        else{
-
-    try{
         return (
-            <div>
+            <Popup
+            trigger= {
+            <div style={{position:'relative', cursor:'pointer'}}>
                 <h2>{homeworld}</h2>
-                <img src={require(`./Images/${homeworld}.png`)} width='60%' alt='homeworld'/>
+               <SwitchPlanetPicture homeworld={homeworld}/> 
             </div>
+            }
+            className='PopupPlanet-style'
+            contentStyle={backgroundStyle}
+            arrowStyle={{background:'linear-gradient(to bottom,#aaa, #222)'}}
+            position='top center'
+            on='click'>
+            <div >
+            <PlanetInfo 
+            terrain={terrain} 
+            climate={climate} 
+            gravity={gravity} 
+            diameter={diameter} 
+            />
+            </div>
+            </Popup>
             )
         }
-     catch(err) {
-         return(
-             <div style={{position:'relative'}}>
-        <h2>{homeworld}</h2>
+    }        
+}
+
+const SwitchPlanetPicture = ({homeworld}) => {
+    try {
+        return(
+        <img src={require(`./Images/${homeworld}.png`)} width='60%' alt='homeworld'/>
+        )
+    }
+    catch(err) {
+        return(
+            <div >
         <img src={require(`./Images/Alderaan.png`)} width='60%' style={{filter: 'grayscale()'}} alt='homeworld'/>
         <h1 className='Popup-questionMark'>?</h1> 
-            </div>
-        )
-        }   
+        </div>
+    )
     }
 }
-        
-}
-    
 
 export default planet;
